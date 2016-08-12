570b6805583b69b8fb56a87ce8d48e2ff50a7296
mishadev <mihailvasileev@gmail.com>
2016-08-12 14:48:28 +0300

    delete all

--- index.php
+++ index.php
@@ -1,24 +1,2 @@
 <?php
 
-trait Params{
-    public function addMageParams(){
-        $this->speed='100';
-        $this->streng='20';
-        $this->agility='10';
-        $this->hp='40';
-        $this->mana='100';
-    }
-}
-
-
-
-class Character{
-    use Params;
-    public function newHero(){
-     $this->addMageParams();
-    }
-}
-
-$char = new Character();
-$char->newHero();
- 
