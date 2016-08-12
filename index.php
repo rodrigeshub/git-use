f9fefbd9a0708a8dca312d4a415da4c74c7d1985
mishadev <mihailvasileev@gmail.com>
2016-08-12 14:32:31 +0300

    final code

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
 
-var_dump(Singleton::get());
-var_dump(Singleton::get());
-var_dump(Singleton::get());
-var_dump(Singleton::get());
-
 trait Params{
     public function addMageParams(){
         $this->speed='100';
@@ -19,7 +14,6 @@ trait Params{
 
 class Character{
     use Params;
-
     public function newHero(){
      $this->addMageParams();
     }
@@ -27,69 +21,4 @@ class Character{
 
 $char = new Character();
 $char->newHero();
-
-
-
-exit;
-abstract class Product
-{
-}
-
-class ProductA extends Product
-{
-}
-
-class ProductB extends Product
-{
-}
-
-abstract class Abstractfactory
-{
-    public function getProdType($type)
-    {
-        if ($type == 'A') {
-            return new ProductA;
-        }
-
-        if ($type == 'B') {
-            return new ProductB;
-        }
-
-    }
-}
-
-class Factory extends Abstractfactory{}
-
-$prod=new Factory();
-var_dump($prod->getProdType('A'));
-
-
-
-
-exit;
-var_dump(Singleton::getInstance());
-
-
-class Singleton
-{
-
-    static private $_instance;
-
-    private function __construct()
-    {
-    }
-
-    private function __clone()
-    {
-    }
-
-    static public function getInstance()
-    {
-        if (self::$_instance === null) {
-            self::$_instance = new self();
-        }
-        return self::$_instance;
-    }
-}
-
-*/
+ 
